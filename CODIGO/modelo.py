c96afabcaefc44897d6bca66f5a44dd26453445a
LukasWolff2002 <lwolff@miuandes.cl>
2024-10-05 12:19:03 -0300

    avance

--- CODIGO/modelo.py
+++ CODIGO/modelo.py
@@ -2,6 +2,7 @@ from lectura_csv import df_filtrado as df
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, confusion_matrix
+from math import exp
 
 # Filtrar los datos para Modo Privado = 1 (privado)
 df_filtrado_privado = df[df['Modo privado'] == 1]
@@ -44,7 +45,19 @@ print('Los valores para y = 1 son:')
 print(f"Intercepto (αa): {intercept}")
 print(f"Coeficientes (θk): {coeficientes}")
 
+#Con esto es posible calcular el valor Va
+#Va = αa + θ1x1 + θ2x2
+#Donde x1 y x2 son los promedos
+x1 = df['Distancia'].mean()
+x2 = df['Ingreso per cápita'].mean()
+Va = (intercept + coeficientes[0][0] * x1 + coeficientes[0][1] * x2)[0]
+print(f'El valor Va es: {Va}')
 
+#COn esto es posible calcular el valor de la probabilidad
+P1 = exp(Va) / (1 + exp(Va))
+P0 = 1 / (1 + exp(Va))
+print(f'La probabilidad de que un viaje sea privado es: {P1=}')
+print(f'La probabilidad de que un viaje sea público es: {P0=}')
 
 
 '''
